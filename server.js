7d22be7b986b2ea14bdae5398fe22676e9a335d7
Thitiwat <win.thitiwat@gmail.com>
2019-06-21 01:51:56 -0700

    update

---
+++ server.js
@@ -0,0 +1,67 @@
+
+/*
+
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user
+
+*/
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+
+// controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+// create connection between server and database
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
+  }
+});
+
+
+// init 
+const app = express();
+
+
+// ---- Middleware ---
+app.use(bodyParser.json()); // to parse data and transform into json
+app.use(cors()); // to allow to connect between client and  webserver
+
+
+// ---- all requests ------
+// app.get('/', (req, res)=>{
+//   res.send(req.);
+// })
+app.get('/', (req, res) => {res.send("It is working")});
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
+
+
+
